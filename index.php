dfc98bd33fc649d79827c12458640ac3a1eaca93
Stijn Keppens <keppens.stijn@gmail.com>
2018-04-30 00:49:43 +0200

    Cleaning up registration form page

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
+
 session_start();
+
+// Connect to the database
 require_once 'dbconnect.php';
 
+// If user is successfully logged in, direct towards home.php 
 if (isset($_SESSION['userSession'])!="") {
  header("Location: home.php");
  exit;
 }
 
+// Evaluate form values when login-form is posted
 if (isset($_POST['btn-login'])) {
  
  $email = strip_tags($_POST['email']);
@@ -15,87 +20,74 @@ if (isset($_POST['btn-login'])) {
  $email = $DBcon->real_escape_string($email);
  $password = $DBcon->real_escape_string($password);
  
- $query = $DBcon->query("SELECT user_id, email, password FROM users WHERE email='$email'");
+ $query = $DBcon->query("SELECT ID, email, password, name FROM users WHERE email='$email'");
+
  $row=$query->fetch_array();
+ $count = $query->num_rows; 
  
- $count = $query->num_rows; // if email/password are correct returns must be 1 row
- 
+ // if password verification is ok and only one row was found in the users table
  if (password_verify($password, $row['password']) && $count==1) {
-  $_SESSION['userSession'] = $row['user_id'];
+
+  // Set session variables
+  $_SESSION['userSession'] = $row['ID'];
+  $_SESSION['userName'] = $row['name'];
+  // direct towards home.php
   header("Location: home.php");
+  
  } else {
+  
+  // Set error login message
   $msg = "<div class='alert alert-danger'>
-     <span class='glyphicon glyphicon-info-sign'></span> &nbsp; Invalid Username or Password !
-    </div>";
+           <span class='glyphicon glyphicon-info-sign'></span> &nbsp; Invalid Username or Password !
+          </div>";
  }
+
+ // Close database connection
  $DBcon->close();
+ 
 }
 ?>
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>FUFANU ADMIN</title>
+<title></title>
 
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-<!-- Custom CSS -->
-<link rel="stylesheet" href="css/main.css">
+ <!-- your css and scripts go here -->
+ <!-- ... -->
 
 
 </head>
 <body>
 
-
-
-
-
-<div class="signin-form">
-
- <div class="container col-md-4 col-md-offset-4">
-     
+ <form class="form-signin" method="post" id="login-form">
+  
+  <h2 class="form-signin-heading">SIGN IN</h2>
+   <?php if(isset($msg)){ echo $msg; } ?>
         
-       <form class="form-signin" method="post" id="login-form">
-      
-        <h2 class="form-signin-heading">SIGN IN</h2>
-            
-        <?php
-  if(isset($msg)){
-   echo $msg;
-  }
-  ?>
-        
-        <div class="form-group">
-        <input type="email" class="form-control" placeholder="Email address" name="email" required />
-        <span id="check-e"></span>
-        </div>
-        
-        <div class="form-group">
-        <input type="password" class="form-control" placeholder="Password" name="password" required />
-        </div>
-       
-        
-        <div class="form-group">
-            <button type="submit" class="btn btn-default" name="btn-login" id="btn-login">
-      <span class="glyphicon glyphicon-log-in"></span> &nbsp; Sign In
+   <div class="form-group">
+    <input type="email" class="form-control" placeholder="Email address" name="email" required />
+    <span id="check-e"></span>
+   </div>
+
+   <div class="form-group">
+    <input type="password" class="form-control" placeholder="Password" name="password" required />
+   </div>
+
+   <div class="form-group">
+    <button type="submit" class="btn btn-default" name="btn-login" id="btn-login">
+    <span class="glyphicon glyphicon-log-in"></span> &nbsp; Sign In
    </button> 
-            
-            <a href="register.php" class="btn btn-default" style="float:right;">Sign up here</a>
-            
-        </div>  
-        
-        
-      
-      </form>
-
-    </div>
     
-</div>
+   <a href="register.php" class="btn btn-default" style="float:right;">Sign up here</a>
 
+   <a href="forgotPassword.php" class="btn btn-default">Forgot your password?</a>
 
+  </div>  
+  
+ </form>
+ 
 </body>
 </html>
